b5d3c31fc5a6e1b0d2feee1c1de562fcd8a01076
Gabriel <64159930+CasualNoLifer@users.noreply.github.com>
2022-11-27 02:04:23 -0600

    Misc
    - AA-12 updated model + attachments + now dyeable + rebalancing
    - Val updated model + attachments + now dyeable + rebalancing
    - Data re-generation
    - Various rebalancing across weapon data and recipes
    - Sound tweaks
    - Removed no longer used textures

--- src/main/java/haloofblocks/projectarsenal/core/registry/ArsenalItems.java
+++ src/main/java/haloofblocks/projectarsenal/core/registry/ArsenalItems.java
@@ -24,7 +24,7 @@ public class ArsenalItems
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ProjectArsenal.MOD_ID);
 
     // ===== GUNS ===== //
-    public static final RegistryObject<Item> AA_TWELVE = registerGun("aa12", false);
+    public static final RegistryObject<Item> AA_TWELVE = registerGun("aa12", true);
     public static final RegistryObject<Item> AUTO_NINE = registerGun("auto9", false);
     public static final RegistryObject<Item> DESERT_EAGLE = registerGun("desert_eagle", false);
     public static final RegistryObject<Item> DP_TWENTY_SEVEN = registerGun("dp27", false);
@@ -33,7 +33,7 @@ public class ArsenalItems
     public static final RegistryObject<Item> MAXIM_NINE = registerGun("maxim9", false);
     public static final RegistryObject<Item> MOSSOU = registerGun("mossou", false);
     public static final RegistryObject<Item> MP_FIVE_A_FOUR = registerGun("mp5a4", false);
-    public static final RegistryObject<Item> VAL = registerGun("val", false);
+    public static final RegistryObject<Item> VAL = registerGun("val", true);
     public static final RegistryObject<Item> SHRIKE = registerGun("shrike", true);
     public static final RegistryObject<Item> GLOCK_TWENTY_ONE = registerGun("glock21", true);
     public static final RegistryObject<Item> P_TWO_FIFTY = registerGun("p250", true);
