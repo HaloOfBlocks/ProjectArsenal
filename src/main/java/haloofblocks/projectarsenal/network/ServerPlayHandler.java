f489b93a2ab18dc4ca0db6761ee26a124fc8f801
Autovw <68228006+Autovw@users.noreply.github.com>
2022-12-03 12:09:51 +0100

    🚧 Implement logic for fire modes

--- src/main/java/haloofblocks/projectarsenal/network/ServerPlayHandler.java
+++ src/main/java/haloofblocks/projectarsenal/network/ServerPlayHandler.java
@@ -18,12 +18,7 @@ public class ServerPlayHandler
         if (!(stack.getItem() instanceof ArsenalGunItem gunItem))
             return;
 
-        Gun modifiedGun = gunItem.getModifiedGun(stack);
         FireMode fireMode = gunItem.getFireMode();
-
-        // Check if gun is set to auto in gun data to allow applying custom fire modes
-        if (!modifiedGun.getGeneral().isAuto())
-            return;
 
         // Change selected fire mode
         gunItem.setSelectedFireMode(fireMode.getFireModes().get(nextFireMode));
